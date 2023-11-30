d150a24c40a4051d3865d80f30b16da322da843e
Pashidy <eldafrancis@yahoo.com>
2023-11-30 11:25:44 -0800

    TASKS

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,14 @@
-# Contents of add_0.py file
-def add(a, b):
-    return a + b
+    #!/usr/bin/python3
 
-# Main program
-if __name__ == "__main__":
+def main():
+    from add_0 import add
+
     a = 1
     b = 2
-    from add_0 import add
+
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
+
+if __name__ == "__main__":
+    main()
+
