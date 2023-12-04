a4b098348e0f1e17e98ea9df5ce0bc19e897f13b
Pashidy <eldafrancis@yahoo.com>
2023-12-03 16:09:16 -0800

    task 5

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]
-    else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return new_list
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    new_list = list(my_list)
+    new_list[idx] = element
+    return new_list
+
+
+if __name__ == "__main__":
+
+    original_list = [1, 2, 3, 4, 5]
+    index = 2
+    new_element = 10
+    modified_list = new_in_list(original_list, index, new_element)
+    print(modified_list)
