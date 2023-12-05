2dd3d661ce41b85c258b5a9432a95f7c7cd05962
Pashidy <eldafrancis@yahoo.com>
2023-12-04 19:24:03 -0800

    tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     """print the number of and list of arguments,"""
     import sys
-    
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
@@ -13,4 +13,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i+1, sys.argv[i+1]))
-
