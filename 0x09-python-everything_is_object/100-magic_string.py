106d06bc2f8cff6560b5fe8093d2dcb531af5285
Pashidy <eldafrancis@yahoo.com>
2024-01-06 11:52:43 -0800

    tasks 0-29

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def magic_string():
+    result = ""
+    for i in range(1, 11):
+        result += "BestSchool" * i
+    return result
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def copy_list(Input_list):
+    return (Input_list[:])
