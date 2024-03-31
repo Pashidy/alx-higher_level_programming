336f6da8f27fb1281940865777a7705ac976c738
Pashidy <eldafrancis@yahoo.com>
2024-03-31 06:24:12 -0700

    Python Network 0 Tasks

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL with the contents of a file in the body, and displays the response body
-
-curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
