9ea8490941ab5ec67d8f8b69d1eab792f35022b2
Pashidy <eldafrancis@yahoo.com>
2024-03-31 05:22:41 -0700

    Python Network 0 Tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes a URL as input, sends a GET request to that URL using curl, and displays the body of the response if the status code is 200
-curl -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
-
+curl -sL "$1"
