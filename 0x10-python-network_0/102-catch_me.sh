506bf85c95997db8fcb22b46c7017d3a462f90f0
Pashidy <eldafrancis@yahoo.com>
2024-03-31 06:15:14 -0700

    Python Network 0 Tasks

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script makes a request to 0.0.0.0:5000/catch_me and causes the server to respond with "You got me!" in the body of the response
+
+curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
