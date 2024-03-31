0141782bf52aac83c42fc388903044c79e9da35d
Pashidy <eldafrancis@yahoo.com>
2024-03-31 05:13:05 -0700

    Python Network 0 Tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes a URL as input, sends a request to that URL using curl, and displays the size of the body of the response in bytes
-curl -sL "$1" | wc -c
-
+curl -s "$1" | wc -c
